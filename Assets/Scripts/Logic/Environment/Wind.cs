2453115f17cf32b3a37aa5a5a6d6da5753686302
Poetic_Programmer <michaelgeorgedunleavy@gmail.com>
2018-06-05 05:00:18 +0100

    Updated Wind state names to adhere to Beaufort scale and set appropriate speed constraints

--- Assets/Scripts/Logic/Environment/Wind.cs
+++ Assets/Scripts/Logic/Environment/Wind.cs
@@ -13,7 +13,7 @@ public class Wind {
     bool transitioning;
 	// Use this for initialization
 	public Wind () {
-        windState = WindState.LIGHT;
+        windState = WindState.CALM;
 
         velocity = new Vector2();
 
@@ -45,36 +45,63 @@ public class Wind {
     private WindState GetWindState()
     {
         var state = windState;
-
-        int roll = Random.Range(0, 10);
-        switch(roll)
+        var minSpeed = 0.0f;
+        var maxSpeed = 0.0f;
+        switch (windState)
         {
-            case 0:
-                state = WindState.HEAVY_FAST;
-                velocity.x = -10;
+            case WindState.CALM:
+                minSpeed = 0.0f;
+                maxSpeed = 0.3f;
                 break;
-            case 1:
-                state = WindState.HEAVY;
-                velocity.x = -10;
+            case WindState.LIGHT_AIR:
+                minSpeed = 1.0f;
+                maxSpeed = 3.0f;
                 break;
-            case 2:
-                state = WindState.MEDIUM_FAST;
-                velocity.x = -10;
+            case WindState.LIGHT_BREEZE:
+                minSpeed = 4.0f;
+                maxSpeed = 7.0f;
                 break;
-            case 3:
-                state = WindState.MEDIUM;
-                velocity.x = -10;
+            case WindState.GENTLE_BREEZE:
+                minSpeed = 8.0f;
+                maxSpeed = 12.0f;
                 break;
-            case 4:
-                state = WindState.LIGHT_FAST;
-                velocity.x = -10;
+            case WindState.MODERATE_BREEZE:
+                minSpeed = 13.0f;
+                maxSpeed = 18.0f;
                 break;
-            case 5:
-                state = WindState.LIGHT;
-                velocity.x = -10;
+            case WindState.FRESH_BREEZE:
+                minSpeed = 19.0f;
+                maxSpeed = 24.0f;
+                break;
+            case WindState.STRONG_BREEZE:
+                minSpeed = 25.0f;
+                maxSpeed = 31.0f;
+                break;
+            case WindState.HIGH_WIND:
+                minSpeed = 32.0f;
+                maxSpeed = 38.0f;
+                break;
+            case WindState.GALE:
+                minSpeed = 39.0f;
+                maxSpeed = 46.0f;
+                break;
+            case WindState.STRONG_GALE:
+                minSpeed = 47.0f;
+                maxSpeed = 54.0f;
+                break;
+            case WindState.STORM:
+                minSpeed = 55.0f;
+                maxSpeed = 63.0f;
+                break;
+            case WindState.VIOLENT_STORM:
+                minSpeed = 64.0f;
+                maxSpeed = 72.0f;
+                break;
+            case WindState.HURRICANE_FORCE:
+                minSpeed = 73.0f;
+                maxSpeed = 81.0f;
                 break;
         }
-
         return state;
     }
 }
--- Assets/Scripts/Logic/Environment/WindState.cs
+++ Assets/Scripts/Logic/Environment/WindState.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum WindState {
-    LIGHT,
-    LIGHT_FAST,
-    MEDIUM,
-    MEDIUM_FAST,
-    HEAVY,
-    HEAVY_FAST
+    CALM,
+    LIGHT_AIR,
+    LIGHT_BREEZE,
+    GENTLE_BREEZE,
+    MODERATE_BREEZE,
+    FRESH_BREEZE,
+    STRONG_BREEZE,
+    HIGH_WIND,
+    GALE,
+    STRONG_GALE,
+    STORM,
+    VIOLENT_STORM,
+    HURRICANE_FORCE
 }
